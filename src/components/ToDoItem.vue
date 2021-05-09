9e60f151397b6af69017180189454cf059582c04
tannercarriere <tannercarriere@email.arizona.edu>
2021-05-09 13:12:38 -0700

    Added edit feature
    Added conditional rendering so we can switch from normal to edit view

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,7 +1,11 @@
 <template>
     <div :class="{ 'todo-container-unchecked': done, 'todo-container-checked': !done }">
-        <input type="checkbox" class="check-box" v-model="done"><span class="todo-object">{{todo}}</span><br/>
-        <input type="button" value="Delete" class="delete-button" v-on:click="rm()">
+      <input v-if="!edit" type="checkbox" class="check-box" v-model="done">
+        <span v-if="!edit" class="todo-object">{{todo}}</span>
+        <span v-else><input id="edit" type="text" :value="todo"></span><br/>
+      <input type="button" value="Delete" class="button" v-on:click="rm()">&nbsp;
+      <input v-if="!edit" type="button" value="Edit" class="button" v-on:click="editSwitch()">
+      <input v-else type="button" value="Submit" class="button" v-on:click="editSwitch()">
     </div>
 </template>
 
@@ -9,8 +13,10 @@
 export default {
   data () {
     let done= false;
+    let edit= false;
     return {
-      done
+      done,
+      edit
     }
   },
   props: {
@@ -19,6 +25,12 @@ export default {
   methods: {
       rm(){
         this.$emit('delete');
+      },
+      editSwitch(){
+        if(this.edit){
+          this.$emit('input')
+        }
+        this.edit = !this.edit;
       }
   }
 }
