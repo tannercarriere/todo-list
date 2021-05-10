fc1a829629ccd524bd663e5f0a8005dbb1af91b8
tannercarriere <tannercarriere@email.arizona.edu>
2021-05-10 13:53:07 -0700

    Cleaned up code, styled better

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,14 +1,16 @@
 <template>
-    <div :class="{ 'todo-container-unchecked': done, 'todo-container-checked': !done }">
+    <div :class="{ 'todo-container unchecked': done, 'todo-container checked': !done }">
       <!-- Conditionally render the checkbox if we're not editing -->
       <input v-if="!edit" type="checkbox" class="check-box" v-model="done">
         <!-- if not editing render the task as plain text-->
         <span v-if="!edit" class="todo-object">{{todo}}</span>
         <!-- if  editing render the task as an input field with the value set to it's previous text-->
-        <span v-else><input id="edit" type="text" :value="todo"></span><br/>
+        <span v-else><input id="edit" type="text" @keyup.enter="editSwitch()" :value="todo"></span><br/>
       <input type="button" value="Delete" class="button" v-on:click="rm()">&nbsp;
-      <input v-if="!edit" type="button" value="Edit" class="button" v-on:click="editSwitch()">
-      <input v-else type="button" value="Submit" class="button" v-on:click="editSwitch()">
+      <span v-if="!edit"><input  type="button" value="Edit" class="button" v-on:click="editSwitch()"></span>
+      <span v-else>
+        <input type="button" value="Submit" class="button" v-on:click="editSwitch()" >
+      </span>
     </div>
 </template>
 
