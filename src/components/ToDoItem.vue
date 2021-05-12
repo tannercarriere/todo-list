e4daf432fc97453213d81192a22195c9d21b127e
tannercarriere <tannercarriere@email.arizona.edu>
2021-05-12 13:21:13 -0700

    Clean up and Note implementation
    Cleaned up the style and added Note component functionality

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,16 +1,20 @@
 <template>
-    <div :class="{ 'todo-container unchecked': done, 'todo-container checked': !done }">
+    <div :class="{ 'todo-container unchecked': done, 'todo-container checked': !done }" >
       <!-- Conditionally render the checkbox if we're not editing -->
       <input v-if="!edit" type="checkbox" class="check-box" v-model="done">
         <!-- if not editing render the task as plain text-->
         <span v-if="!edit" class="todo-object">{{todo}}</span>
         <!-- if  editing render the task as an input field with the value set to it's previous text-->
-        <span v-else><input id="edit" type="text" @keyup.enter="editSwitch()" :value="todo"></span><br/>
+        <span v-else><input id="edit" type="text" v-on:keyup.enter="editSwitch()" :value="todo"></span><br/>
+
       <input type="button" value="Delete" class="button" v-on:click="rm()">&nbsp;
-      <span v-if="!edit"><input  type="button" value="Edit" class="button" v-on:click="editSwitch()"></span>
+      <span v-if="!edit">
+        <input  type="button" value="Edit" class="button" v-on:click="editSwitch()">
+      </span>
       <span v-else>
         <input type="button" value="Submit" class="button" v-on:click="editSwitch()" >
       </span>
+      <input type="button" value="Note" class="button" v-on:click="noteRequest()" >
     </div>
 </template>
 
@@ -33,9 +37,12 @@ export default {
       },
       editSwitch(){
         if(this.edit){//this will only emit if in edit mode
-          this.$emit('input')
+          this.$emit('edit')
         }
         this.edit = !this.edit;
+      },
+      noteRequest(){
+        this.$emit('notes');
       }
   }
 }
