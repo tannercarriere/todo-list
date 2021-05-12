5952480457d0c73cedb730b9c5730dd9de87a438
tannercarriere <tannercarriere@email.arizona.edu>
2021-05-12 13:17:20 -0700

    Basic implementation
    Generates a Note box filled with the associated message

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,13 +1,33 @@
 <template>
-
+    <div class="note-box">
+        <h4>Notes on "{{this.task}}"</h4>
+        <p v-if="!edit" class="message-box">
+            {{this.message}}
+        </p>
+        <textarea v-else class="message-box" id="edit-note" type="text" v-on:keyup.enter="editSwitch()" :value="this.message"/>
+        <input type="button" class="button" value="Add/Edit Note" @click="editSwitch()"/>
+    </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    let edit= false;
+    return {
+      edit
+    }
   },
-  props: {},
-  methods: {}
+  props: {
+    task: {required: true, type: String},
+    message: {required: true, type: String}
+  },
+  methods: {
+    editSwitch(){
+        if(this.edit){//this will only emit if in edit mode
+          this.$emit('edit')
+        }
+        this.edit = !this.edit;
+      }
+  }
 }
 </script>
