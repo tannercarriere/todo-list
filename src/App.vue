7d362d9a221dd104a28d4dc55d68ef373c61fcb2
tannercarriere <tannercarriere@email.arizona.edu>
2021-05-12 13:16:24 -0700

    Added support for Note component
    Can now generate a Note specific to each todo item

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,34 @@
       <h1 class="title">TO-DO LIST</h1>
       <ul class="list-container" >
         <li>
-          <input ref="push" class="input" type="text" @keyup.enter="push()" v-model="todo">
+          <input class="input" type="text" @keyup.enter="push()" v-model="todo">
         </li>
         <li v-for="(item, index) in todos" :key="item.id">
-          <to-do-item :todo="item.task" :id="index" @delete="pop(index)" @input="edit(index)" @add-cat="addCat(index)"/>
+          <to-do-item :todo="item.task" :id="index" @delete="pop(index)" @edit="edit(index)" @notes="showNote(index)"/>
         </li>
+        <div ref="notes"></div>
       </ul>
   </div>
 </template>
 
 <script>
 import ToDoItem from './components/ToDoItem.vue'
+import Note from './components/Note.vue'
+import Vue from 'vue'
 export default {
   name: 'App',
   data () {
     return {
       todo: "",
       todos: [],
+      notes: []
     }
   }, mounted(){
       if(localStorage.getItem("todos")){//if we have values in local storage set those as default
         this.todos = JSON.parse(localStorage.getItem("todos"));
+      }
+      if(localStorage.getItem("notes")){//if we have values in local storage set those as default
+        this.notes = JSON.parse(localStorage.getItem("notes"));
       }
   },
   watch:{
@@ -31,6 +38,13 @@ export default {
       deep: true,
       handler(){//if the todos array changes store the change
         localStorage.setItem('todos', JSON.stringify(this.todos));
+      }
+    },
+    notes:{
+      deep: true,
+      handler(){
+        console.log(this.notes)
+        localStorage.setItem('notes', JSON.stringify(this.notes));
       }
     }
     
@@ -40,20 +54,42 @@ export default {
       this.todos.push(
         {
           task:this.todo, 
-          id:Date.now(),
+          id:Date.now()
         }
       ); //the id allows us to have tasks of duplicate names
+      this.notes.push(
+        {
+          task:this.todo, 
+          message:""
+        }
+      );
       this.todo = "";
     },
     pop(index){
       this.todos.splice(index, 1);//drop the item at the given index
+      this.notes.splice(index, 1);
     },
     edit(index){
       this.todos[index].task = document.getElementById("edit").value; // change the task at the given index
+    },
+    showNote(index){
+      console.log("lmao")
+      let ComponentClass = Vue.extend(Note);
+      let instance = new ComponentClass({
+        propsData: {
+          task: this.notes[index].task,
+          message: this.notes[index].message
+        }
+      });
+      instance.$mount();
+      this.$refs.notes.appendChild(instance.$el);
+      this.$on.edit('edit', function(){
+
+      });
     }
   },
   components:{
-      ToDoItem
+      ToDoItem,
   }
 }
 </script>
@@ -93,6 +129,7 @@ export default {
     color: rgba(255,255,255,.5);
   }
   .list-container {
+    display: inline-block;
     width: 90%;
     margin: auto;
   }
@@ -110,5 +147,16 @@ export default {
     border: none;
     margin-top: .25rem;
     margin-right: .1rem;
+  }
+  .note-box{
+    background-color: rgba(255,255,255,.5);
+    width: 24rem;
+    height: 25rem;
+    padding: 1rem;
+  }
+  .message-box{
+    background-color: rgba(255,255,255,.5);
+    width: 95%;
+    height: 90%;
   }
 </style>
